5e3edabb77a5226fc983dca497d6f71df157bd9c
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2016-09-07 19:44:36 +0430

    پیاده سازی متدها اضافه شده

--- src/core/Content.js
+++ src/core/Content.js
@@ -8,75 +8,126 @@ angular.module('pluf')
  * @memberof pluf
  * @ngdoc factory
  * @name PContent
- * @description
- * ساختار داده‌ای محتوی را ایجاد می‌کند. این ساختار داده‌ای شامل اطلاعات کلی از محتوی است که
- * از این میان می‌توان به موارد زیر اشاره کرد:
- *
+ * @description ساختار داده‌ای محتوی را ایجاد می‌کند. این ساختار داده‌ای شامل
+ *              اطلاعات کلی از محتوی است که از این میان می‌توان به موارد زیر
+ *              اشاره کرد:
+ * 
  * @attr {integer} id
  * @attr {string} name
  * @attr {string} mimetype
  * @attr {integer} tenant
  */
 .factory('PContent', function($http, $httpParamSerializerJQLike, $q, PObject) {
+
+	function _initContent(scope) {
+		scope.link = '/api/saascms/content/' + scope.id + '/download';
+	}
+
 	var pContent = function() {
 		PObject.apply(this, arguments);
+		_initContent(this);
 	};
- 	pContent.prototype = new PObject();
+	pContent.prototype = new PObject();
+
 	/**
 	 * محتوی را به روز می‌کند
-	 *
+	 * 
 	 * @memberof PContent
 	 * @return {promise} محتوی جدید ایجاد شده
 	 */
-	pContent.prototype.update = function(){
-		// TODO:maso, 1395: به روز کردن محتوی
+	pContent.prototype.update = function() {
+		var scope = this;
+		return $http({
+			method : 'POST',
+			url : '/api/saascms/content/' + this.id,
+			data : $httpParamSerializerJQLike(scope),
+			headers : {
+				'Content-Type' : 'application/x-www-form-urlencoded'
+			}
+		}).then(function(data) {
+			scope.setData(data.data);
+			_initContent(scope);
+			return scope;
+		});
 	};
 
 	/**
 	 * محتوی را حذف می‌کند
+	 * 
 	 * @memberof PContent
 	 * @return {promise} محتوی حذف شده
 	 */
-	pContent.prototype.remove = function(){
-		// TODO:maso, 1395: حذف محتوی
+	pContent.prototype.remove = function() {
+		var scope = this;
+		return $http({
+			method : 'DELETE',
+			url : '/api/saascms/content/' + this.id
+		}).then(function() {
+			delete scope.id;
+			return scope;
+		});
 	};
 
 	/**
-	 * مقدار محتوی را تعیین می‌کند که معمولا برای گرفتن محتوی ساختار یافته و رشته‌ها مناسب
-	 * است. در سایر موارد استفاده از پیوند محتوی بهتر است.
+	 * مقدار محتوی را تعیین می‌کند که معمولا برای گرفتن محتوی ساختار یافته و
+	 * رشته‌ها مناسب است. در سایر موارد استفاده از پیوند محتوی بهتر است.
+	 * 
 	 * @memberof PContent
 	 * @return {promise} مقدار محتوی
 	 */
-	pContent.prototype.value = function(){
+	pContent.prototype.value = function() {
 		// TODO: maso, 1395: محتوی صفحه را می‌دهد
 		// if(this._cvalue()){
-		// 	var deferred = $q.defer();
-		// 	deferred.resolve(this._cvalue());
-		// 	return deferred.promise;
+		// var deferred = $q.defer();
+		// deferred.resolve(this._cvalue());
+		// return deferred.promise;
 		// }
 		return $http({
-			method: 'GET',
-			url: '/api/saascms/content/'+this.id+'/download'
-		}).then(function(res){
+			method : 'GET',
+			url : this.link
+		}).then(function(res) {
 			// scope._setCvalue(res.data);
 			return res.data;
 		});
 	};
+
 	/**
 	 * مقدار جدیدی را برای این محتوی تعیین می‌کند.
+	 * 
 	 * @memberof PContent
-	 * @param  {object} d مقدار جدید برای محتوی
-	 * @return {promise}   محتوی به روز شده
+	 * @param {object}
+	 *            data مقدار جدید برای محتوی
+	 * @return {promise} محتوی به روز شده
 	 */
-	pContent.prototype.setValue = function(d){
+	pContent.prototype.setValue = function(newValue) {
 		var scope = this;
 		return $http({
-			method:'POST',
-			url: '/api/saascms/content/'+this.id+'/download',
-			data: d,
-		}).then(function(){
+			method : 'POST',
+			url : this.link,
+			data : newValue,
+		}).then(function() {
 			return scope;
 		});
 	};
- 	return pContent;
- });
+
+	/**
+	 * یک فایل را به عنوان مقدار بار می‌کند
+	 * 
+	 * ورودی باید فایل جاوسکریپت باشه.
+	 * 
+	 * @param file
+	 * @returns
+	 */
+	pContent.prototype.upload = function(file) {
+		var fd = new FormData();
+		fd.append('file', file);
+		return $http.post(this.link, fd, {
+			transformRequest : angular.identity,
+			headers : {
+				'Content-Type' : undefined
+			}
+		});
+	};
+
+	return pContent;
+});
