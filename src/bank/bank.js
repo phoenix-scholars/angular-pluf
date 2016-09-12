5c9fc4edb1858c280806656ff2aca8f5f50226d7
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2016-09-12 08:53:57 +0430

    تست بانک و سرویس آن تکمیل شد

--- src/bank/bank.js
+++ src/bank/bank.js
@@ -14,7 +14,7 @@ angular.module('pluf')
  * سرویس انجام کارهای بانکی.
  * 
  * مهم‌ترین کاری که برای این سرویس در نظر گرفته شده است ایجاد یک پرداخت جدید در
- * سیستم است. 
+ * سیستم است.
  * 
  * <pre><code>
  * // اجرای دستور
@@ -23,8 +23,8 @@ angular.module('pluf')
  * });
  * </code></pre>
  * 
- * بعد از اینکه پرداخت در سیستم ایجاد شد کاربر می تونه برای پرداخت آن از درگاه تعیین 
- * شده اقدام کنه.
+ * بعد از اینکه پرداخت در سیستم ایجاد شد کاربر می تونه برای پرداخت آن از درگاه
+ * تعیین شده اقدام کنه.
  * 
  * Gate way link is placed in receipt.
  * 
@@ -39,96 +39,231 @@ angular.module('pluf')
  * A receipt is accessable with secure_id.
  * 
  */
-.service('$bank', function(/* $http, $q, PaginatorPage */) {
-	/**
-	 * Creates new receipt
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PReceipt> created receipt
-	 */
-	this.createReceipt = function() {
+.service(
+		'$bank',
+		function($http, $q, PaginatorPage, PBank, PGate, PReceipt) {
+			var _banks = {};
+			var _gates = {};
+			var _receipts = {};
 
-	};
+			// TODO: maso, 1395: add to PObject
+			function _paginatorParams(paginatorParam) {
+				if (angular.isUndefined(paginatorParam) || !angular.isFunction(paginatorParam.getParameter)) {
+					return {};
+				}
+				return paginatorParam.getParameter();
+			}
+			
+			/*
+			 * گرفتن یک محتوی
+			 */
+			function _bank(id) {
+				return _banks[id];
+			}
+			/*
+			 * بازیابی یک محتوی نامدار
+			 */
+			function _retbank(id, data) {
+				var bank = _banks[id];
+				if (bank) {
+					bank.setData(data);
+				} else {
+					bank = new PBank(data);
+					_banks[id] = bank;
+				}
+				return bank;
+			}
+			
+			function _gate(id){
+				return _gates[id];
+			}
 
-	/**
-	 * Gets receipt detail by secure id
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PReceipt> created receipt
-	 */
-	this.receipt = function() {
+			function _retgate(id, data) {
+				var gate = _gates[id];
+				if (gate) {
+					gate.setData(data);
+				} else {
+					gate = new PGate(data);
+					_gates[id] = gate;
+				}
+				return gate;
+			}
+			
+			function _receipt(id){
+				return _receipts[id];
+			}
+			
+			function _retreceipt(id, data) {
+				var receipt = _receipts[id];
+				if (receipt) {
+					receipt.setData(data);
+				} else {
+					receipt = new PReceipt(data);
+					_receipts[id] = receipt;
+				}
+				return receipt;
+			}
+			
+			/**
+			 * Creates new receipt
+			 * 
+			 * @memberof $bank
+			 * @return Promise<PReceipt>
+			 * createdreceipt
+			 * 
+			 */
+			this.createReceipt = function() {
 
-	};
+			};
 
-	/**
-	 * Gets receipt detail
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PReceipt> created receipt
-	 */
-	this.receiptById = function() {
-		
-	};
+			/**
+			 * Gets receipt detail by secure id
+			 * 
+			 * @memberof $bank
+			 * @return Promise<PReceipt>
+			 * createdreceipt
+			 * 
+			 */
+			this.receipt = function() {
 
-	/**
-	 * Lists all receipts
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PaginatorPage<PReceipt>> created receipt
-	 */
-	this.receipts = function() {
+			};
 
-	};
+			/**
+			 * Gets receipt detail
+			 * 
+			 * @memberof $bank
+			 * @return Promise<PReceipt>
+			 * createdreceipt
+			 * 
+			 */
+			this.receiptById = function(id) {
+				var receipt = _receipt(id);
+				if(receipt){
+					var deferred = $q.defer();
+					deferred.resolve(receipt);
+					return deferred.promise;
+				}
+				return $http({
+					method : 'GET',
+					url : '/api/bank/receipt/'+id,
+				}).then(function(res){
+					return _retreceipt(res.data.id, res.data);
+				});
+			};
 
-	/**
-	 * Creates new gate
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PGate> created gate
-	 */
-	this.createGate = function() {
+			/**
+			 * Lists all receipts
+			 * 
+			 * @memberof $bank
+			 * @return Promise<PaginatorPage<PReceipt>>
+			 * createdreceipt
+			 * 
+			 */
+			this.receipts = function() {
 
-	};
+			};
 
-	/**
-	 * Gets a gate
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PGate> a gate
-	 */
-	this.gate = function() {
+			/**
+			 * Creates new gate
+			 * 
+			 * @memberof $bank
+			 * @return Promise<PGate> created gate
+			 */
+			this.createGate = function() {
 
-	};
+			};
 
-	/**
-	 * Lists all gates
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PaginatorPage<PGate>> gates list
-	 */
-	this.gates = function() {
+			/**
+			 * Gets a gate
+			 * 
+			 * @memberof $bank
+			 * @return Promise<PGate> a gate
+			 */
+			this.gate = function(id) {
+				var gate = _gate(id);
+				if(gate){
+					var deferred = $q.defer();
+					deferred.resolve(gate);
+					return deferred.promise;
+				}
+				return $http({
+					method : 'GET',
+					url : '/api/bank/backend/'+id,
+				}).then(function(res){
+					return _retgate(res.data.id, res.data);
+				});
+			};
 
-	};
+			/**
+			 * Lists all gates
+			 * 
+			 * @memberof $bank
+			 * @param paginatorParam
+			 * @return Promise<PaginatorPage<PGate>> gates list
+			 */
+			this.gates = function(paginatorParam) {
+				return $http({
+					method : 'GET',
+					url : '/api/bank/backend/find',
+					params : _paginatorParams(paginatorParam)
+				}).then(
+						function(res) {
+							var data = res.data;
+							var page = new PaginatorPage(data);
+							page.items = [];
+							for (var i = 0; i < data.counts; i++) {
+								page.items.push(_retgate(
+										data.items[i].type, data.items[i]));
+							}
+							return page;
+						});
+			};
 
-	/**
-	 * Gets bank detail
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PBank> 
-	 */
-	this.bank = function() {
+			/**
+			 * Gets bank detail
+			 * 
+			 * @memberof $bank
+			 * @return Promise<PBank>
+			 */
+			this.bank = function(type) {
+				var bank = _bank(type);
+				if(bank){
+					var deferred = $q.defer();
+					deferred.resolve(bank);
+					return deferred.promise;
+				}
+				return $http({
+					method : 'GET',
+					url : '/api/bank/engine/'+type,
+				}).then(function(res){
+					bank = _retbank(res.data.type, res.data);
+					return bank;
+				});
+			};
+			
+			/**
+			 * Gets bank list
+			 * 
+			 * 
+			 * @memberof $bank
+			 * @return Promise<PaginatorPage<PGate>> gates list
+			 */
+			this.banks = function(paginatorParam) {
+				return $http({
+					method : 'GET',
+					url : '/api/bank/engine/find',
+					params : _paginatorParams(paginatorParam)
+				}).then(
+						function(res) {
+							var data = res.data;
+							var page = new PaginatorPage(data);
+							page.items = [];
+							for (var i = 0; i < data.counts; i++) {
+								page.items.push(_retbank(
+										data.items[i].type, data.items[i]));
+							}
+							return page;
+						});
+			};
 
-	};
-	
-	/**
-	 * Gets bank list
-	 * 
-	 * 
-	 * @memberof $bank
-	 * @return Promise<PaginatorPage<PGate>> gates list
-	 */
-	this.banks = function() {
-
-	};
-
-});
+		});
--- test/spec/bank/bank.js
+++ test/spec/bank/bank.js
@@ -5,14 +5,25 @@
 
 describe('Bank module test: $bank', function() {
 	var $bank;
+	var $rootScope;
+	var $httpBackend;
+	var $timeout;
+	var PaginatorParameter;
 
 	beforeEach(function() {
 		module('pluf');
 	});
 
 	beforeEach(function() {
-		inject(function(_$bank_) {
+		inject(function(_$bank_, _$rootScope_, _$httpBackend_, _$timeout_,
+				_PaginatorParameter_) {
 			$bank = _$bank_;
+
+			// Concurent test
+			$rootScope = _$rootScope_;
+			$httpBackend = _$httpBackend_;
+			$timeout = _$timeout_;
+			PaginatorParameter = _PaginatorParameter_;
 		});
 	});
 
@@ -31,5 +42,61 @@ describe('Bank module test: $bank', function() {
 		// bank api
 		expect(angular.isFunction($bank.bank)).toBe(true);
 		expect(angular.isFunction($bank.banks)).toBe(true);
+	});
+
+	/*
+	 * جستجو گرفتن بانک تست شده است.
+	 */
+	it('Bank find bank tests with params', function(done) {
+		var pag = new PaginatorParameter();
+		$bank.banks(pag).then(function(banks) {
+			expect(banks).not.toBeNull();
+			done();
+		});
+
+		$httpBackend.expect('GET', '/api/bank/engine/find').respond(200, {
+			'items' : [],
+			'counts' : 0,
+			'current_page' : 0,
+			'items_per_page' : 2,
+			'page_number' : 1
+		});
+		expect($httpBackend.flush).not.toThrow();
+		$rootScope.$apply();
+	});
+
+	it('Bank find bank tests without params', function(done) {
+		$bank.banks().then(function(banks) {
+			expect(banks).not.toBeNull();
+			done();
+		});
+
+		$httpBackend.expect('GET', '/api/bank/engine/find').respond(200, {
+			'items' : [],
+			'counts' : 0,
+			'current_page' : 0,
+			'items_per_page' : 2,
+			'page_number' : 1
+		});
+		expect($httpBackend.flush).not.toThrow();
+		$rootScope.$apply();
+	});
+
+	it('Bank get bank tests', function(done) {
+		var type = 'xxxx';
+		$bank.bank(type)//
+		.then(function(bank) {
+			expect(bank).not.toBeNull();
+			done();
+		});
+		$httpBackend.expect('GET', '/api/bank/engine/' + type)//
+		.respond(200, {
+			'type' : type,
+			'title' : type,
+			'description' : 'Mellat Payment Service',
+			'symbol' : 'mellat'
+		});
+		expect($httpBackend.flush).not.toThrow();
+		$rootScope.$apply();
 	});
 });
