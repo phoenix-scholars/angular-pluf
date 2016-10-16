96d3ed9ab55aade3ed5dba6ba36ea87ac6c498ed
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2016-10-17 02:06:07 +0330

    ساختار بسته کلود و سرویس آن به روز رسانی شده است

--- dist/angular-pluf.js
+++ dist/angular-pluf.js
@@ -3282,6 +3282,38 @@ angular.module('pluf')
 
 			/**
 			 * 
+			 */
+			this.createUpdate = function(params) {
+				params.headers = {
+					'Content-Type' : 'application/x-www-form-urlencoded'
+				};
+				return function(objectData) {
+					var scope = this;
+					params.data = $httpParamSerializerJQLike(objectData);
+					return $http(params)//
+					.then(function(res) {
+						scope.setData(res.data);
+						return scope;
+					});
+				};
+			};
+
+			/**
+			 * 
+			 */
+			this.createDelete = function(params) {
+				return function() {
+					var scope = this;
+					return $http(params)//
+					.then(function(res) {
+						scope.setData(res.data);
+						return scope;
+					});
+				};
+			};
+
+			/**
+			 * 
 			 * @memberof $pluf
 			 * @param {Object}
 			 *            params
@@ -3417,6 +3449,118 @@ angular.module('pluf')
   this.process = function(){};
 });
 
+/*
+ * Copyright (c) 2015 Phoenix Scholars Co. (http://dpq.co.ir)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+'use strict';
+
+angular.module('pluf')
+/**
+ * دستورها و دستگیره‌ها
+ * 
+ * تمام دستورهای و دستگیره‌هایی که در رابطه با این ماژول وجود دارد را در این
+ * پرونده اضافه شده است. این دستورها برای کاربردهای عمومی به کار می‌رود.
+ */
+.run(function($act) {
+	/**
+	 * اضافه کردن دستورها و دستگیره‌ها
+	 */
+	$act.command({
+		id : 'tenant.lunch',
+		category : 'saas',
+	})
+	// run spa
+	.command({
+		id : 'spa.lunch',
+		category : 'saas',
+	});
+});
+/*
+ * Copyright (c) 2015 Phoenix Scholars Co. (http://dpq.co.ir)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+'use strict';
+
+angular.module('pluf')
+/**
+ * دستگیره‌ها
+ * 
+ * تمام دستورهای و دستگیره‌هایی که در رابطه با این ماژول وجود دارد را در این
+ * پرونده اضافه شده است. این دستورها برای کاربردهای عمومی به کار می‌رود.
+ */
+.run(function($window, $act, $saas, PException) {
+	/**
+	 * اضافه کردن دستورها و دستگیره‌ها
+	 */
+	$act
+	// Lunch an application
+	.handler({
+		command : 'tenant.lunch',
+		handle : function() {
+			if (arguments.length < 1) {
+				throw new PException('tenant not found');
+			}
+			var tenantId = arguments[0];
+			return $saas.get(tenantId)//
+			.then(function(tenant) {
+				return tenant.defaultApplication();
+			}).then(function(app) {
+				return app.run();
+			});
+		}
+	})
+	// run spa
+	.handler({
+		command : 'spa.lunch',
+		handle : function() {
+			if (arguments.length < 1) {//
+				throw new PException('application not found');
+			}
+			var spaId = arguments[0];
+			return $saas.session()//
+			.then(function(tenant) {
+				return tenant.app(spaId);
+			}).then(function(app) {
+				return app.run();
+			});
+		}
+	});
+});
 /*
  * Copyright (c) 2015 Phoenix Scholars Co. (http://dpq.co.ir)
  * 
@@ -4106,305 +4250,116 @@ angular.module('pluf')
 
 angular.module('pluf')
 /**
- * دستورها و دستگیره‌ها
- * 
- * تمام دستورهای و دستگیره‌هایی که در رابطه با این ماژول وجود دارد را در این
- * پرونده اضافه شده است. این دستورها برای کاربردهای عمومی به کار می‌رود.
+ * @ngdoc service
+ * @name $saas
+ * @memberof pluf.saas
+ * @description مدیریت ملک و نرم افزارها را انجام می‌دهد.
  */
-.run(function($act) {
-	/**
-	 * اضافه کردن دستورها و دستگیره‌ها
-	 */
-	$act.command({
-		id : 'tenant.lunch',
-		category : 'saas',
-	})
-	// run spa
-	.command({
-		id : 'spa.lunch',
-		category : 'saas',
+.service('$saas', function($http, PTenant, PSpa, PObjectCache, $pluf) {
+
+	var _tenantCache = new PObjectCache(function(data) {
+		return new PTenant(data);
 	});
-});
-/*
- * Copyright (c) 2015 Phoenix Scholars Co. (http://dpq.co.ir)
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-'use strict';
 
-angular.module('pluf')
-/**
- * دستگیره‌ها
- * 
- * تمام دستورهای و دستگیره‌هایی که در رابطه با این ماژول وجود دارد را در این
- * پرونده اضافه شده است. این دستورها برای کاربردهای عمومی به کار می‌رود.
- */
-.run(function($window, $act, $saas, PException) {
-	/**
-	 * اضافه کردن دستورها و دستگیره‌ها
-	 */
-	$act
-	// Lunch an application
-	.handler({
-		command : 'tenant.lunch',
-		handle : function() {
-			if (arguments.length < 1) {
-				throw new PException('tenant not found');
-			}
-			var tenantId = arguments[0];
-			return $saas.get(tenantId)//
-			.then(function(tenant) {
-				return tenant.defaultApplication();
-			}).then(function(app) {
-				return app.run();
-			});
-		}
-	})
-	// run spa
-	.handler({
-		command : 'spa.lunch',
-		handle : function() {
-			if (arguments.length < 1) {//
-				throw new PException('application not found');
-			}
-			var spaId = arguments[0];
-			return $saas.session()//
-			.then(function(tenant) {
-				return tenant.app(spaId);
-			}).then(function(app) {
-				return app.run();
-			});
-		}
+	var _spaCache = new PObjectCache(function(data) {
+		return new PSpa(data);
 	});
+
+	/**
+	 * نمونه جاری را تعیین می‌کند. به صورت پیش فرض اجرای هر نرم افزار روی یک ملک
+	 * اجرا می‌شود این فراخوانی ملکی را تعیین می‌کند که نرم افزار جاری روی آن
+	 * کار می‌کند.
+	 * 
+	 * @memberof $saas
+	 * @return {permision(PTenant)} ملک جاری را تعیین می‌کند.
+	 */
+	this.session = function() {
+		return $http.get('/api/tenant')//
+		.then(function(res) {
+			return _tenantCache.restor(res.data);
+		});
+	};
+
+	/**
+	 * فهرست تمام ملک‌هایی را که کاربر به آنها دسترسی دارد را تعیین می‌کند.
+	 * 
+	 * @memberof $saas
+	 * @param {PaginatorParameter}
+	 *            paginatorParameter پارامترهای مورد استفاده در صفحه بندی
+	 * @return {promise<PaginatorPage<PTenant>>} فهرست ملک‌ها به صورت صفحه
+	 *         بندی
+	 */
+	this.tenants = $pluf.createFind({
+		method : 'GET',
+		url : '/api/tenant/find',
+	}, _tenantCache);
+
+	/**
+	 * ملک تعیین شده با شناسه را برمی‌گرداند.
+	 * 
+	 * @memberof $saas
+	 * @param {integer}
+	 *            id شناسه ملک مورد نظر
+	 * @return {promise<PTenant>} ملک تعیین شده.
+	 */
+	this.tenant = $pluf.createGet({
+		url : '/api/tenant/{id}',
+		method : 'GET'
+	}, _tenantCache);
+
+	/**
+	 * یک ملک جدید ایجاد می‌کند و ساختار ایجاد شده برای آنرا به عنوان نتیجه
+	 * برمی‌گرداند.
+	 * 
+	 * @memberof $saas
+	 * @param {Struct}
+	 *            tenantData ساختار داده‌ای ملک
+	 * @return {promise<PTenant>} مکل ایجاد شده
+	 */
+	this.newTenant = $pluf.createNew({
+		method : 'POST',
+		url : '/api/tenant/new',
+	}, _tenantCache);
+
+	/**
+	 * فهرست تمام نرم افزارهایی را تعیین می‌کند که برای ملک جاری در دسترس است.
+	 * 
+	 * @memberof $saas
+	 * @param {PaginatorParameter}
+	 *            paginatorParameter پارامترهای مورد استفاده در صفحه بندی
+	 * @return {promise<PaginatorPage<PSpa>>} فهرست نرم افزارها
+	 */
+	this.spas = $pluf.createFind({
+		method : 'GET',
+		url : '/api/spa/find',
+	}, _spaCache);
+
+	/**
+	 * نرم افزار معادل با شناسه ورودی را بازیابی می‌کند.
+	 * 
+	 * @memberof $saas
+	 * @param {integer}
+	 *            id شناسه نرم افزار
+	 * @return {promise<PSpa>} نرم‌افزار معادل
+	 */
+	this.spa = $pluf.createGet({
+		url : '/api/spa/{id}',
+		method : 'GET'
+	}, _spaCache);
+
+	/**
+	 * یک نرم افزار جدید در سیستم ایجاد می‌کند.
+	 * 
+	 * @memberof $saas
+	 * @param {Struct}
+	 *            spa ساختار داده‌ای یک spa
+	 * @return {promise<PSpa} نرم‌افزار معادل ایجاد شده
+	 */
+	this.newSpa = $pluf.createNew({
+		method : 'POST',
+		url : '/api/spa/new'
+	}, _spaCache);
 });
-/*
- * Copyright (c) 2015 Phoenix Scholars Co. (http://dpq.co.ir)
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-'use strict';
-
-angular.module('pluf')
-		/**
-		 * @ngdoc service
-		 * @name $saas
-		 * @memberof pluf.saas
-		 * @description مدیریت ملک و نرم افزارها را انجام می‌دهد.
-		 */
-		.service(
-				'$saas',
-				function($http, $httpParamSerializerJQLike, $q, $window, $act,
-						$usr, PTenant, PSpa, PaginatorParameter, PaginatorPage,
-						PObjectCache) {
-					var contentType = 'application/x-www-form-urlencoded';
-
-					var _tenantCache = new PObjectCache(function(data) {
-						return new PTenant(data);
-					});
-
-					var _spaCache = new PObjectCache(function(data) {
-						return new PSpa(data);
-					});
-
-					/**
-					 * نمونه جاری را تعیین می‌کند. به صورت پیش فرض اجرای هر نرم
-					 * افزار روی یک ملک اجرا می‌شود این فراخوانی ملکی را تعیین
-					 * می‌کند که نرم افزار جاری روی آن کار می‌کند.
-					 * 
-					 * @memberof $saas
-					 * @return {permision(PTenant)} ملک جاری را تعیین می‌کند.
-					 */
-					this.session = function() {
-						return $http.get('/api/tenant')//
-						.then(function(res) {
-							return _tenantCache.restor(res.data);
-						});
-					};
-
-					/**
-					 * فهرست تمام ملک‌هایی را که کاربر به آنها دسترسی دارد را
-					 * تعیین می‌کند.
-					 * 
-					 * @memberof $saas
-					 * @param {PaginatorParameter}
-					 *            paginatorParameter پارامترهای مورد استفاده در
-					 *            صفحه بندی
-					 * @return {promise<PaginatorPage<PTenant>>} فهرست ملک‌ها
-					 *         به صورت صفحه بندی
-					 */
-					this.tenants = function(paginatorParameter) {
-						var param = {};
-						if (paginatorParameter) {
-							param = paginatorParameter.getParameter();
-						}
-						return $http({
-							method : 'GET',
-							url : '/api/tenant/find',
-							params : param
-						}).then(
-								function(res) {
-									var page = new PaginatorPage(res.data);
-									page.items = [];
-									for (var i = 0; i < page.counts; i++) {
-										var item = res.data.items[i];
-										page.items.push(_tenantCache.restor(
-												item.id, item));
-									}
-									return page;
-								});
-					};
-
-					/**
-					 * ملک تعیین شده با شناسه را برمی‌گرداند.
-					 * 
-					 * @memberof $saas
-					 * @param {integer}
-					 *            id شناسه ملک مورد نظر
-					 * @return {promise<PTenant>} ملک تعیین شده.
-					 */
-					this.tenant = function(id) {
-						if (_tenantCache.contains(id)) {
-							var deferred = $q.defer();
-							deferred.resolve(_tenantCache.get(id));
-							return deferred.promise;
-						}
-						return $http.get('/api/tenant/' + id)//
-						.then(function(res) {
-							return _tenantCache.restor(res.data.id, res.data);
-						});
-					};
-
-					/**
-					 * یک ملک جدید ایجاد می‌کند و ساختار ایجاد شده برای آنرا به
-					 * عنوان نتیجه برمی‌گرداند.
-					 * 
-					 * @memberof $saas
-					 * @param {Struct}
-					 *            tenantData ساختار داده‌ای ملک
-					 * @return {promise<PTenant>} مکل ایجاد شده
-					 */
-					this.newTenant = function(t) {
-						return $http({
-							method : 'POST',
-							url : '/api/tenant/new',
-							data : $httpParamSerializerJQLike(t),
-							headers : {
-								'Content-Type' : contentType
-							}
-						})//
-						.then(function(res) {
-							return _tenantCache.restor(res.data.id, res.data);
-						});
-					};
-
-					/**
-					 * فهرست تمام نرم افزارهایی را تعیین می‌کند که برای ملک جاری
-					 * در دسترس است.
-					 * 
-					 * @memberof $saas
-					 * @param {PaginatorParameter}
-					 *            paginatorParameter پارامترهای مورد استفاده در
-					 *            صفحه بندی
-					 * @return {promise<PaginatorPage<PSpa>>} فهرست نرم
-					 *         افزارها
-					 */
-					this.spas = function(paginatorParameter) {
-						var param = {};
-						if (paginatorParameter) {
-							param = paginatorParameter.getParameter();
-						}
-						return $http({
-							method : 'GET',
-							url : '/api/spa/find',
-							params : param
-						})//
-						.then(
-								function(res) {
-									var page = new PaginatorPage(res.data);
-									page.items = [];
-									for (var i = 0; i < page.counts; i++) {
-										var item = res.data.items[i];
-										page.items.push(_spaCache.restore(
-												item.id, item));
-									}
-									return page;
-								});
-					};
-
-					/**
-					 * نرم افزار معادل با شناسه ورودی را بازیابی می‌کند.
-					 * 
-					 * @memberof $saas
-					 * @param {integer}
-					 *            id شناسه نرم افزار
-					 * @return {promise<PSpa>} نرم‌افزار معادل
-					 */
-					this.spa = function(id) {
-						if (_spaCache.contains(id)) {
-							var deferred = $q.defer();
-							deferred.resolve(_spaCache.get(id));
-							return deferred.promise;
-						}
-						return $http.get('/api/spa/' + id)//
-						.then(function(res) {
-							return _spaCache.restor(res.data.id, res.data);
-						});
-					};
-
-					/**
-					 * یک نرم افزار جدید در سیستم ایجاد می‌کند.
-					 * 
-					 * @memberof $saas
-					 * @param {Struct}
-					 *            spa ساختار داده‌ای یک spa
-					 * @return {promise<PSpa} نرم‌افزار معادل ایجاد شده
-					 */
-					this.newSpa = function(detail) {
-						return $http({
-							method : 'POST',
-							url : '/api/spa/new',
-							data : $httpParamSerializerJQLike(detail),
-							headers : {
-								'Content-Type' : contentType
-							}
-						})//
-						.then(function(res) {
-							return _spaCache.restor(res.data.id, res.data);
-						});
-					};
-				});
 
 /*
  * Copyright (c) 2015 Phoenix Scholars Co. (http://dpq.co.ir)
