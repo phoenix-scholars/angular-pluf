e3f77fbbc5b1c745fa48c95b5085cc529b2f9e53
hadi <hadi@dpq.co.ir>
2018-02-10 00:36:39 +0330

    Two method 'clearFilters' and 'clearSorters' are added

--- src/core/PaginatorParameter.js
+++ src/core/PaginatorParameter.js
@@ -128,6 +128,10 @@ angular.module('pluf')
 			this._init_sorts();
 			return this;
 		},
+		clearSorters: function(){
+			this.sortMap = {};
+		},
+		
 		setFilter : function($key, $value) {
 			if(!$value){				
 				this.removeFilter($key, $value);
@@ -150,6 +154,10 @@ angular.module('pluf')
 			this._init_filters();
 			return this;
 		},
+		clearFilters: function(){
+			this.filterMap = {};
+		},
+		
 		getParameter : function() {
 			return this.param;
 		},
